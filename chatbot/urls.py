b10fd50610bbd5a3884a316b9bbf05a1f6036f89
aagya shrestha <aagyashrestha66666@gmail.com>
2025-03-15 22:43:54 +0545

    Add chatbot app with basic structure

---
+++ chatbot/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import chatbot, chatbot_page
+
+urlpatterns = [
+    path('', chatbot_page, name='chatbot_page'),  # Serves chatbot HTML page
+    path('chat/', chatbot, name='chatbot'),  # API endpoint for chatbot responses
+]
