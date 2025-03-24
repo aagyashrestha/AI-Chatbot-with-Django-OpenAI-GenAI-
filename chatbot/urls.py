0f8a24507f21ec0c0049f1ca81aea5b5f402947e
aagya shrestha <aagyashrestha66666@gmail.com>
2025-03-24 11:10:46 +0545

    API-based chatbot that remembers the last 7 conversations for each user

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,3 +1,10 @@
+# from django.urls import path
+# from .views import chatbot, chatbot_page
+
+# urlpatterns = [
+#     path('', chatbot_page, name='chatbot_page'),  # Serves chatbot HTML page
+#     path('chat/', chatbot, name='chatbot'),  # API endpoint for chatbot responses
+# ]
 from django.urls import path
 from .views import chatbot, chatbot_page
 
